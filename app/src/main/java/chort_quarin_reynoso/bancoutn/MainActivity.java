64b63d19a95648a9520e1762b1a8e053bce5edd5
FedericoQuarin <federicoquarin2@gmail.com>
2022-09-09 19:11:16 -0300

    Initial commit

---
+++ app/src/main/java/chort_quarin_reynoso/bancoutn/MainActivity.java
@@ -0,0 +1,14 @@
+package chort_quarin_reynoso.bancoutn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
